6c42e151d14a227553b48939015296a37fb928eb
Jerald Banalan <20-06378@g.batstate-u.edu.ph>
2023-08-16 21:09:11 +0800

    Try

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { FirstContent } from './Components/FirstContent';
 import { SecondContent } from './Components/SecondContent';
 import ThirdContent from './Components/ThirdContent';
 import AddContent from './Components/AddContent';
-
+import Form_Event  from './Components/form';
 
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
       <SecondContent />
       <ThirdContent />
       <AddContent/>
-
+      <Form_Event/>
     </div>
   );
 }
